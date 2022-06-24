9f4fe1eaadafd304d4e67f96cf6da100363d5807
Bollók Ákos Dániel <bollokakos@debian-BULLSEYE-live-builder-AMD64>
2022-06-24 18:58:01 +0200

    Created main route components and included them in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Typography } from '@mui/material';
 import { Routes, Route } from "react-router-dom";
+import Home from './components/home/Home';
+import Store from './components/store/Store';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 
 const App = () => {
     return (
         <Routes>
-            <Route path='./' />
-            <Route path='./store' />
-            <Route path='./login' />
-            <Route path='./signup' />
+            <Route path='./' element={<Home />} />
+            <Route path='./store' element={<Store />} />
+            <Route path='./login' element={<Login />} />
+            <Route path='./signup' element={<Signup />} />
         </Routes>
     )
 }
